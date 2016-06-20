7268ba96e947a7934ec7aad7ab347ffa7b526d21
LiCheng <244344094@qq.com>
2016-06-20 18:29:08 +0800

    二维码生成

---
+++ QRCodeTool/QRCodeTool/QRCodeTool/QRCodeTool.h
@@ -0,0 +1,27 @@
+//
+//  QRCodeTool.h
+//  QRCodeTool
+//
+//  Created by LiCheng on 16/6/20.
+//  Copyright © 2016年 Li_Cheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface QRCodeTool : NSObject
+
+
+/**
+ *  生成二维码
+ *
+ *  @param message 二维码信息
+ *  @param width   宽
+ *  @param height  高
+ *
+ *  @return 二维码图片
+ */
++(UIImage *)createQRCodeWithMessage:(NSString *)message size:(CGFloat)size;
+
+
+@end
--- QRCodeTool/QRCodeTool/ViewController.m
+++ QRCodeTool/QRCodeTool/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "QRCodeTool.h"
 
 @interface ViewController ()
 
@@ -24,4 +25,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - 生成二维码
+- (IBAction)createQRCode:(id)sender {
+    
+    UIImageView *iamge = [[UIImageView alloc] initWithFrame:(CGRectMake(100, self.view.frame.size.height - 250, 200, 200))];
+    
+    iamge.image = [QRCodeTool createQRCodeWithMessage:@"1234" size:200];
+    
+    [self.view addSubview:iamge];
+    
+}
+
+#pragma mark - 扫描二维码
+- (IBAction)scanQRCode:(id)sender {
+    
+}
 @end
